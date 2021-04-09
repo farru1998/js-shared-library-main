e294dcd35c530c6b61c9030f62dbdbc36b47f122
farru1998 <44400940+farru1998@users.noreply.github.com>
2021-04-09 10:15:28 +0500

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -12,10 +12,7 @@ class Docker implements Serializable {
     def buildDockerImage(String imageName) {
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-credential', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.docker.withRegistry('https://registry.hub.docker.com', 'docker-credential') {
-                //script.docker.build("$imageName", '.').push()
-                script{
-                    docker.build registry + ":$BUILD_NUMBER"
-                }
+                script.docker.build("$BUILD_NUMBER", '.').push()
             }
         }
     }
