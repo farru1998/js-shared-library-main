1ef3b6a25b688763ce9cd5e88350291641920e6c
farru1998 <44400940+farru1998@users.noreply.github.com>
2021-04-09 10:54:12 +0500

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,10 +1,13 @@
 #!/usr/bin/env groovy
+import jenkins.model.Jenkins
 package com.example
 
 class Docker implements Serializable {
 
     def script
 
+    def upStreamBuild = Jenkins.getInstance().getItemByFullName(item.fullName).getBuildByNumber(last_job_num)
+    
     Docker(script) {
         this.script = script
     }
@@ -12,7 +15,7 @@ class Docker implements Serializable {
     def buildDockerImage(String imageName) {
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-credential', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.docker.withRegistry('https://registry.hub.docker.com', 'docker-credential') {
-                script.docker.build("$imageName:${currentBuild.number}",'.').push()
+                script.docker.build("$imageName:$upStreamBuild",'.').push()
                 // dockerApp = dockerLib.buildDockerImage(IMAGE_NAME, "", "${workspace}/${DOCKERFILE_PATH}", imageTagsList[0], buildArgs)
             }
         }
